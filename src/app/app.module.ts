8366cc435336c6a40c221187391289dd16e7a465
Mike Huang <wellwind@mail2000.com.tw>
2021-03-14 10:00:48 +0800

    將樣板內容搬移到元件內

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { TagsComponent } from './tags/tags.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ArticlesComponent,
+    TagsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
